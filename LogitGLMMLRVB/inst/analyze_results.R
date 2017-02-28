40ff3701f52f62777db7303c1cc129b9f52f73e1
Ryan Giordano <rgiordan@gmail.com>
2017-02-27 18:30:22 -0800

    Save influence functions

--- LogitGLMMLRVB/inst/analyze_results.R
+++ LogitGLMMLRVB/inst/analyze_results.R
@@ -107,13 +107,12 @@ prior_sens_cast <- dcast(
 ##################
 # Influence functions
 
-
 beta_comp <- 1
 beta_funs <- GetBetaImportanceFunctions(beta_comp, vp_opt, pp, lrvb_results)
 
 num_mc_draws <- 5
 beta_influence_results <- GetVariationalInfluenceResults(
-  num_draws = 200,
+  num_draws = 2000,
   DrawImportanceSamples = beta_funs$DrawU,
   GetImportanceLogProb = beta_funs$GetULogDensity,
   GetLogQGradTerms = function(u_draws) { beta_funs$GetLogQGradTerms(u_draws, num_mc_draws, normalize=TRUE) },
@@ -125,8 +124,8 @@ beta_influence_results <- GetVariationalInfluenceResults(
 draws_mat <- vb_results$draws_mat
 param_draws <- draws_mat[, beta_comp]
 mcmc_funs <- GetMCMCInfluenceFunctions(param_draws, beta_funs$GetLogPrior)
-mcmc_worst_case <- sapply(1:ncol(draws_mat), function(ind) { mcmc_funs$GetMCMCWorstCase(draws_mat[, ind]) })
-
+GetMCMCWorstCaseColumn <- function(col) { mcmc_funs$GetMCMCWorstCase(draws_mat[, col]) }
+mcmc_worst_case <- sapply(1:ncol(draws_mat), GetMCMCWorstCaseColumn)
 
 # Compare
 worst_case_df <- rbind(
@@ -136,46 +135,43 @@ worst_case_df <- rbind(
                      method="lrvb", metric=paste("beta", beta_comp, sep=""))
 )
 
-worst_case_cast <- dcast(worst_case_df, par + component + group + metric ~ method, value.var="val")
-ggplot(worst_case_cast) +
-  geom_point(aes(x=mcmc, y=lrvb, color=par, shape=factor(component)), size=2) +
-  geom_abline(aes(slope=1, intercept=0)) +
-  expand_limits(x=0, y=0)
+
+###################################
+# Get graphs of influence functions
+
+GetInfluenceDF <- function(u, influence, gbar, log_posterior, log_prior, worst_u, method, metric) {
+  data.frame(u=u, influence=influence, gbar=gbar,
+             log_posterior=log_posterior, log_prior=log_prior,
+             worst_u=worst_u,
+             method=method, metric=metric)  
+}
 
 # ind <- mp_indices$beta_e_vec[beta_comp]
-ind <- mp_indices$beta_e_vec[4]
-
-mcmc_influence <- mcmc_funs$GetMCMCInfluence(g_draws=draws_mat[, ind])
-mcmc_conditional_mean_diff <- mcmc_funs$GetConditionalMeanDiff(g_draws=draws_mat[, ind])
-
-grid.arrange(
-  ggplot() +
-    geom_line(aes(x=beta_influence_results$u_draws, y=beta_influence_results$influence_fun[, ind], color="lrvb"), lwd=2) +
-    geom_line(aes(x=param_draws, y=mcmc_influence, color="mcmc"), lwd=2)
-,  
-  ggplot() +
-    geom_line(aes(x=beta_influence_results$u_draws, y=exp(beta_influence_results$log_q - beta_influence_results$log_prior), color="lrvb"), lwd=2) +
-    geom_line(aes(x=param_draws, y=exp(log(mcmc_funs$dens_at_draws$y) - beta_funs$GetLogPrior(param_draws)), color="mcmc"), lwd=2)
- , 
-  ggplot() +
-    geom_line(aes(x=beta_influence_results$u_draws, y=beta_influence_results$log_q_grad_terms[ind, ], color="lrvb"), lwd=2) +
-    geom_line(aes(x=param_draws, y=mcmc_conditional_mean_diff, color="mcmc"), lwd=2)
-  , ncol=3
-)
-
-
-# Look at the draws underlying the conditional expectation estimates
+# ind <- mp_indices$beta_e_vec[4]
+ind <- mp_indices$mu_e
+metric <- sprintf("beta%d_on_ind%d", beta_comp, ind)
 g_draws <- draws_mat[, ind]
-ggplot() +
-  geom_point(aes(x=param_draws, y=g_draws, color="draws"), alpha=0.3, size=2) +
-  geom_line(aes(x=param_draws, y=mcmc_conditional_mean_diff + mean(g_draws), color="mcmc"), lwd=3) +
-  geom_line(aes(x=beta_influence_results$u_draws, y=beta_influence_results$log_q_grad_terms[ind, ]+ mean(g_draws), color="lrvb"), lwd=3)
-  
-# Look at the worst-case perturbation
-ggplot() + 
-  geom_point(aes(x=beta_influence_results$u_draws, y=beta_influence_results$worst_case_u[, ind], color="ustar")) +
-  geom_point(aes(x=beta_influence_results$u_draws, y=exp(beta_influence_results$log_prior), color="prior"))
-  
+
+mcmc_worst <- mcmc_funs$GetMCMCWorstCaseResults(g_draws)
+mcmc_influence_df <- GetInfluenceDF(
+  u=mcmc_funs$param_draws,
+  influence=mcmc_worst$mcmc_influence,
+  gbar=mcmc_funs$GetConditionalMeanDiff(g_draws),
+  log_posterior=log(mcmc_funs$dens_at_draws),
+  log_prior=mcmc_funs$log_prior_at_draws,
+  worst_u=mcmc_worst$worst_u,
+  method="mcmc",
+  metric=metric)
+
+vb_influence_df <- GetInfluenceDF(
+  u=beta_influence_results$u_draws,
+  influence=beta_influence_results$influence_fun[, ind],
+  gbar=beta_influence_results$log_q_grad_terms[ind, ],
+  log_posterior=beta_influence_results$log_q,
+  log_prior=beta_influence_results$log_prior,
+  worst_u=beta_influence_results$worst_case_u[, ind],
+  method="lrvb",
+  metric=metric)
 
 
 #############################
@@ -191,21 +187,62 @@ mfvb_sd <- GetMomentParametersFromVector(vp_mom, sqrt(diag(mfvb_cov)), FALSE)
 results <- SummarizeResults(mcmc_sample, vp_mom, mfvb_sd, lrvb_sd)
 
 if (save_results) {
-  influence_cast_sub <- sample_n(influence_cast, 5000)
   mcmc_time <- as.numeric(vb_results$stan_results$mcmc_time, units="secs")
   vb_time <- as.numeric(vb_results$fit_time, units="secs")
   num_mcmc_draws <- nrow(as.matrix(vb_results$stan_results$stan_sim))
   num_logit_sims <- length(vb_results$opt$std_draws)
   num_obs <- vb_results$stan_results$stan_dat$N
   beta_dim <- vb_results$stan_results$stan_dat$K
-  save(results, influence_cast_sub, prior_sens_cast, mp_opt,
+  save(results, prior_sens_cast, mp_opt,
        mcmc_time, vb_time, num_mcmc_draws, pp, num_logit_sims, num_obs, beta_dim,
+       worst_case_df, vb_influence_df, mcmc_influence_df,
        file=results_file)
 }
+
+
+########################################
+# Graphs and analysis
 
 stop("Graphs follow -- not executing.")
 
 
+
+if (FALSE) {
+  grid.arrange(
+    ggplot() +
+      geom_line(data=vb_influence_df, aes(x=u, y=influence, color=method), lwd=2) +
+      geom_line(data=mcmc_influence_df, aes(x=u, y=influence, color=method), lwd=2) 
+    , 
+    ggplot() +
+      geom_line(data=vb_influence_df, aes(x=u, y=gbar, color=method), lwd=2) +
+      geom_line(data=mcmc_influence_df, aes(x=u, y=gbar, color=method), lwd=2) 
+    , 
+    ggplot() +
+      geom_line(data=vb_influence_df, aes(x=u, y=exp(log_posterior), color=method), lwd=2) +
+      geom_line(data=mcmc_influence_df, aes(x=u, y=exp(log_posterior), color=method), lwd=2) 
+    , ncol=3
+  )
+  
+  
+  # Look at the draws underlying the conditional expectation estimates
+  ggplot() +
+    geom_point(aes(x=param_draws, y=g_draws, color="draws"), alpha=0.3, size=2) +
+    geom_line(data=mcmc_influence_df, aes(x=u, y=gbar + mean(g_draws), color=method), lwd=2) 
+
+  # Look at the worst-case perturbation
+  ggplot() + 
+    geom_point(aes(x=beta_influence_results$u_draws, y=beta_influence_results$worst_case_u[, ind], color="ustar")) +
+    geom_point(aes(x=beta_influence_results$u_draws, y=exp(beta_influence_results$log_prior), color="prior"))
+  
+}
+
+
+
+worst_case_cast <- dcast(worst_case_df, par + component + group + metric ~ method, value.var="val")
+ggplot(worst_case_cast) +
+  geom_point(aes(x=mcmc, y=lrvb, color=par, shape=factor(component)), size=2) +
+  geom_abline(aes(slope=1, intercept=0)) +
+  expand_limits(x=0, y=0)
 
 # Overall
 
